b6b13cfbced39544b0b9c91a10d9c7f970bcbc20
sebastiansopala <s.sopala@gmail.com>
2023-12-31 06:28:38 +0100

    test viewModel

--- app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.triviaapp.screens.QuestionViewModel
 import com.example.triviaapp.ui.theme.TriviaAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -19,30 +19,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TriviaAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//                TriviaHome()
+                Questions(viewModel = hiltViewModel())
             }
         }
     }
 }
 
+//@Composable
+//fun TriviaHome(viewModel: QuestionViewModel = hiltViewModel()) {
+//    Questions(viewModel)
+//}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Questions(viewModel: QuestionViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+
+    if (viewModel.data.value.isLoading == true) {
+        Log.d("22Test", "List size: ${questions?.size} coz its loading")
+
+        CircularProgressIndicator()
+    } else {
+        Log.d("22Test", "List size: ${questions?.size}, loaded")
+
+        questions?.forEachIndexed { index, questionItem ->
+            Log.d("22Test", "$index, ${questionItem.question}")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TriviaAppTheme {
-        Greeting("Android")
+
     }
 }
